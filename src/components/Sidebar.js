9ec67e4de9d3b9105169085fb5e0a7eaae8ed66e
Karamveer200 <>
2021-05-03 00:41:30 -0700

    sidebar implemented

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { Avatar, IconButton, Button } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import "./css/Sidebar.css";
 import SidebarOption from "./SidebarOption";
 
@@ -15,7 +22,23 @@ function Sidebar() {
       >
         Compose
       </Button>
-      <SidebarOption Icon={InboxIcon} title="Inbox" Number={52} />
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        Number={52}
+        Selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" Number={52} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" Number={52} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" Number={52} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" Number={52} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" Number={52} />
+
+      <SidebarOption Icon={ExpandMoreIcon} title="More" Number />
+
+      <div className="sidebar_footer">
+        <div className="sidebar_footerIcons"></div>
+      </div>
     </div>
   );
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./css/SidebarOption.css";
 
-function SidebarOption({ Icon, title, Number }) {
+function SidebarOption({ Icon, title, Number, Selected }) {
   return (
-    <div className="sidebarOption">
+    <div className={`sidebarOption ${Selected && "sidebarOption_active"}`}>
       <Icon />
       <h3>{title}</h3>
       <p>{Number}</p>
